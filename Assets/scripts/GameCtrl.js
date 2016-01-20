9c537531d82f32ac7a7318d8dc795db7d598eba3
caed04 <caed04@gmail.com>
2016-01-20 14:41:22 -0500

    Ajout des éléments à l'interface de jeu
    Modification au script de GameCtrl et scCasque

--- Assets/scripts/GameCtrl.js
+++ Assets/scripts/GameCtrl.js
@@ -4,10 +4,12 @@ public var nbDiamants:int= 0;
 public var nbRubis:int= 0;
 public var nbMaxDiamants:int= 10;
 public var nbMaxRubis:int= 20;
+public var viesDark:int=1;
 public var dynamite:boolean=false;
 
 public var diamants: UnityEngine.UI.Text;
 public var rubis: UnityEngine.UI.Text;
+public var vies: UnityEngine.UI.Text;
 
 function Start () {
 
@@ -15,6 +17,7 @@ function Start () {
 
 function Update () {
 
-	rubis.text= "Rubis: " + nbRubis.ToString() + "/"+nbMaxRubis;
-	diamants.text= "Diamants: " + nbDiamants.ToString() + "/" +nbMaxDiamants;
+	rubis.text= nbRubis.ToString() + "/"+nbMaxRubis;
+	diamants.text= nbDiamants.ToString() + "/" +nbMaxDiamants;
+	vies.text= viesDark .ToString();
 }

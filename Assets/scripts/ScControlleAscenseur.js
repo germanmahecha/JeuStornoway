1f72a688115e6641414033025b6dd0f49c305c2d
JBlancoA <jorgeblancoa@gmail.com>
2016-01-22 15:32:07 -0500

    Ascenseur : animations, levier : animation, plate-forme

--- Assets/scripts/ScControlleAscenseur.js
+++ Assets/scripts/ScControlleAscenseur.js
@@ -1,23 +1,59 @@
 ﻿#pragma strict
-internal var animator:Animator;
+//internal var animator:Animator;
+public var animateur:Animator;
+public var dark:GameObject;
+public var ascenseur_ferme:GameObject;
+//public var ascenseur:GameObject;
+
+
+// Variable qui contient le script du GameCtrl;
+
+
 
 function Start () {
-	animator=GetComponent(Animator);
+
+
+	animateur=GetComponent.<Animator>();
+
 }
 
 function Update () {
 	
 }
 
+//GetComponent.<Animator>().enabled = false;
+
+
+ascenseur_ferme.SetActive (false);
 function OnTriggerEnter2D(other: Collider2D	){
-	Debug.Log("Entre");
-	animator.SetBool("Fermer",true);
-	animator.SetBool("Arrete",false);
+	if(other.gameObject.tag == 'Dark')
+	{
+	//Debug.Log("Entre");
+		animateur.SetBool("Enter", true);
+		//animateur.SetBool("sort", true);
+		dark.transform.parent = this.transform;
+		ascenseur_ferme.SetActive (true);
+
+	}
+
+
 }
 
 function OnTriggerExit2D(other: Collider2D	){
-	Debug.Log("Sorti");
-	animator.SetBool("Ouvrir",true);
-	animator.SetBool("Fermer",false);
-	animator.SetBool("Arrete",true);
+	if(other.gameObject.tag == 'Dark')
+	{
+	//Debug.Log("Entre");
+		//animateur.SetBool("Enter", true);
+		//animateur.SetBool("sort", true);
+		dark.transform.parent = null;
+
+	}
+
+
 }
+
+
+
+
+
+
---
+++ Assets/scripts/ScLevier.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+public var porte_barre:GameObject;
+
+
+function Start () {
+
+
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(other: Collider2D	){
+	if(other.gameObject.tag == 'Dark')
+	{
+	//Debug.Log("Entre");
+	Destroy(porte_barre);
+
+
+	}
+
+}

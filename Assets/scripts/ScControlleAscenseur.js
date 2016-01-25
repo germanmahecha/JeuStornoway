eb7a5e4dae170de2ff1ae58a2aa6bb1f2add5520
JBlancoA <jorgeblancoa@gmail.com>
2016-01-24 20:20:12 -0500

    correction d'un erreur del ascenseur, placements des éléments
    fermeture de l’ascenseur pendant Dark est dedans, est ouverture quand
    l’animation de l’ascenseur est fini. placements des éléments et
    stalactites,

--- Assets/scripts/ScControlleAscenseur.js
+++ Assets/scripts/ScControlleAscenseur.js
@@ -2,8 +2,10 @@
 //internal var animator:Animator;
 public var animateur:Animator;
 public var dark:GameObject;
-public var ascenseur_ferme:GameObject;
-//public var ascenseur:GameObject;
+public var ascenseur_ferme_droite:GameObject;
+public var ascenseur_ferme_gauche:GameObject;
+
+
 
 
 // Variable qui contient le script du GameCtrl;
@@ -21,19 +23,20 @@ function Update () {
 	
 }
 
-//GetComponent.<Animator>().enabled = false;
 
 
-ascenseur_ferme.SetActive (false);
+
+ascenseur_ferme_droite.SetActive (false);
+ascenseur_ferme_gauche.SetActive (false);
 function OnTriggerEnter2D(other: Collider2D	){
 	if(other.gameObject.tag == 'Dark')
 	{
-	//Debug.Log("Entre");
+	Debug.Log("Entre");
 		animateur.SetBool("Enter", true);
 		//animateur.SetBool("sort", true);
 		dark.transform.parent = this.transform;
-		ascenseur_ferme.SetActive (true);
-
+		ascenseur_ferme_droite.SetActive (true);
+		ascenseur_ferme_gauche.SetActive (true);
 	}
 
 
@@ -47,10 +50,20 @@ function OnTriggerExit2D(other: Collider2D	){
 		//animateur.SetBool("sort", true);
 		dark.transform.parent = null;
 
+
 	}
 
 
 }
+
+function desactiver_portes_ascenseur(){
+		ascenseur_ferme_droite.SetActive (false);
+		ascenseur_ferme_gauche.SetActive (false);
+}
+
+
+
+
 
 
 

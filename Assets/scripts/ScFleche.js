c7d6e252c7b3537a69fdaf4b425c4eb24e17ac94
thuytien83 <tienvothuy@yahoo.com>
2016-02-05 05:21:37 -0500

    Son/Flèche
    Ajouter les sons pour les boutons.
    Ajouter l'animation de flèche.
    Ajouter le code pour que la flèche apparait.

---
+++ Assets/scripts/ScFleche.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+public var fleche:GameObject;
+
+fleche.SetActive (false);
+
+function OnTriggerEnter2D(other: Collider2D)
+{
+	if(other.gameObject.tag=='Dark')
+	{
+		fleche.SetActive (true);
+	}
+}
+
+function OnTriggerExit2D(other: Collider2D	)
+{
+	if(other.gameObject.tag == 'Dark')
+	{  
+	    fleche.SetActive (false);
+	}
+
+}
--- Assets/scripts/scMenu.js
+++ Assets/scripts/scMenu.js
@@ -1,22 +1,43 @@
-﻿/* Démarrer l'écran de Jouer/Options/Instructions
+﻿/*
+ Démarrer l'écran de Niveaux/Options/Instructions
 
+Source de son:
+Multimédia Button Click 006
+Auteur: mckinneysound's
+http://www.freesfx.co.uk/rx2/mp3s/2/2690_1329133083.mp3
 */
+private var son:AudioSource;
+
+function Start()
+{
+   son=GetComponent.<AudioSource>();
+
+}
+
+function Awake()
+{
+     DontDestroyOnLoad (transform.gameObject);
+
+}
 
 
 function StartJouer()
 {
+    son.Play();
     Application.LoadLevel("MenuNiveaux");
 
 }
 
 function StartOptions()
 {
+    son.Play();
     Application.LoadLevel("MenuOptions");
 
 }
 
 function StartInstruction()
 {
+    son.Play();
     Application.LoadLevel("MenuInstructions");
 
 }
